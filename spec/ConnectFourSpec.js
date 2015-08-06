bed92adaa6624aacd37439629030d9512ae11724
Del Stevens <del.stevens@gmail.com>
2015-08-07 11:10:03 +1200

    add turn logic

--- spec/ConnectFourSpec.js
+++ spec/ConnectFourSpec.js
@@ -10,14 +10,33 @@ describe("ConnectFour", function(){
 	});
 
 	it("board should have 7 columns and 6 rows", function() {
-		expect(board.rowsColumns).toBeDefined();
+		board.rows[5][6] = " "
+		expect(board.rows[5][6]).toBeDefined();
 	});
 
 	it("play piece should be created", function(){
 		expect(board.playPiece).toBeDefined();
-	})
+	});
 
 	it("should register something within the column", function() {
-		expect(board.playPiece("blue", 1)).toEqual(board.rowsColumns[1][5] = "blue")
+		board.playPiece(1)
+		expect(board.rows[5][1]).toBeDefined();
 	});
+
+
+	it("Registers a down win condition on winboard, if start position is 2,0"), function() {
+		
+		expect(board.checkWin(2, 0, "red") == [2,0, "down"])
+	});
+
+	xit("should register an down_win condition on winboard, if start position is 1,2")
+
+	xit("should register a down_left_win condition on winboard if start position is 3,2")
+
+	xit("should register a down_right_win condition on winboard if start position is 0,3")
+
+
+
 });
+
+
--- src/ConnectFour.js
+++ src/ConnectFour.js
@@ -1,27 +1,174 @@
-var Board = function(color, column) {
-	var rows = new Array(6);
-		for (var i = 0; i < 6; i++) {
-		rows[i] = new Array(7);
-	this.board = rows;
-	this.color = color;
-	this.column = column;
+
+
+var Board = function (){
+	var rows = new Array (6)
+	for (var i = 0; i<6; i++){
+		rows[i] = new Array (7)
 	};
+	this.rows = rows
+	this.redTurn = true
+	this.colour = "red"
+	this.gameScoreRed = 0;
+	this.gameScoreBlue = 0;
 };
 
-
-Board.prototype.rowsColumns = function() {
-	console.log(this.board)
+Board.prototype.newGame = function() {
+	for (i = 0; i< 7; i++) {
+		for (j = 0; j<6; j++) {
+			this.rows[j][i] = null
+		}
+	}
 }
 
 
-Board.prototype.playPiece = function() {
-	console.log(this.color)
-	console.log(this.column)
-	console.log(this.board)
+Board.prototype.playPiece = function(column) {
+    for (var i in this.rows ) {
+        if (!this.rows[i][column]) {
+            available = i
+        }
+    }
 
-
+    if (this.redTurn) {
+    	this.colour = "red"
+    	this.redTurn = false
+    } else {
+    	this.colour = "blue"
+    	this.redTurn = true
+    }
+    this.rows[available][column] = this.colour
+    this.checkWin(available, column, this.colour)
 }
 
-board = new Board('blue', 1);
-board.rowsColumns();
-board.playPiece();
+
+Board.prototype.showPiece = function (row, col) {
+	console.log(this.rows[row][col])
+}
+
+Board.prototype.checkWin = function (row, col, colour) {
+	
+	var resultsArray = new Array
+
+	//check horizontal win
+	var horizArray = new Array
+	for (var i = 0; i<7; i++){
+		horizArray.push(this.rows[row][i])
+	}
+	homeCol = checkFourColours(horizArray, colour)
+	if (homeCol > -1) {
+		resultsArray.push(row)
+		resultsArray.push(homeCol)
+		resultsArray.push("right")
+		return(resultsArray)
+	}
+
+	//check vertical win
+	var vertArray = new Array
+	for (var i = row; i<6; i++){
+		vertArray.push(this.rows[i][col])
+	}
+	homeCol = checkFourColours(vertArray, colour)
+	if (homeCol > -1) {
+		resultsArray.push(row)
+		resultsArray.push(col)
+		resultsArray.push("down")
+		return(resultsArray)
+	}
+
+	var diagUpRight = new Array
+	//calculate the left base point on this diagonal
+	var startCol = col;
+	var startRow = row
+	while ((startCol > 0 ) && (startRow <5)) {
+		startCol --
+		startRow ++
+	}
+	var tempRow = startRow
+	for (i =startCol; i<7; i++) {
+		if (tempRow > -1) {
+			diagUpRight.push(this.rows[tempRow][i])
+			tempRow --
+		}
+	} 
+	homeCol = checkFourColours(diagUpRight, colour)
+	if (homeCol > -1) {
+		resultsArray.push(startRow)
+		resultsArray.push(startCol)
+		resultsArray.push("upRight")
+		return(resultsArray)
+	}
+
+	var diagUpLeft = new Array
+
+	//calculate the right base point on this diagonal
+	startCol = col
+	startRow = row
+	while ((startCol < 6) && (startRow <5)) {
+		startCol ++
+		startRow ++
+	}
+
+	var tempRow = startRow
+	var tempCol = startCol
+	for (i =tempCol; i>-1; i--) {
+		if (tempRow > -1) {
+			diagUpLeft.push(this.rows[tempRow][i])
+			tempRow --
+		}
+	} 
+
+	homeCol = checkFourColours(diagUpRight, colour)
+	if (homeCol > -1) {
+		resultsArray.push(startRow)
+		resultsArray.push(startCol)
+		resultsArray.push("upLeft")
+		return(resultsArray)
+	}
+
+
+	function checkFourColours (inArray, colour) {
+		var sequenceCount = 0
+		var startCell = -1
+		var outArray = new Array
+		for (var cell = 0; cell < inArray.length; cell++) {
+			if (inArray[cell] == colour) {
+				if (startCell == -1) {
+					startCell = cell
+				}
+				sequenceCount ++
+				if (sequenceCount > 3) {
+					return startCell
+				}
+			} else {
+				sequenceCount = 0
+				startCell = -1
+			}
+		}
+		return -1
+	}
+}
+
+
+
+
+
+board = new Board
+console.log(board)
+
+	board.playPiece(0)
+	board.playPiece(1)
+	board.playPiece(0)
+	board.playPiece(1)
+	board.playPiece(0)
+	board.playPiece(1)
+	board.playPiece(0)
+	board.playPiece(1)
+console.log(board)	
+	board.newGame()
+
+	board.playPiece(1)
+	board.playPiece(0)
+	board.playPiece(1)
+
+console.log(board)
+
+

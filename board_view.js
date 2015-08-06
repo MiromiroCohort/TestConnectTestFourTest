9e9576bf683fcef725ffd9d0b10dcae93b4a88bd
IonaCS <apprentice@StMac8.local>
2015-08-07 11:20:29 +1200

    add possibly functional win message with new game button

--- board_view.js
+++ board_view.js
@@ -1,13 +1,46 @@
 $( document ).ready(function(){
 
+  var newBoard = function(){
+    $('.cell').toggleClass('empty')
+  }
+
+
+
   $('.cell').click(function(){
       var col = $( this ).attr('class').split(' ')[2].slice(-1);
       var colInt = parseInt(col)
       board.playPiece(colInt);
-    if(Board.redTurn == false){
+    if(Board.colour == "blue"){
       $( this ).toggleClass("blue")
     } else {
       $( this ).toggleClass("red")
     }
   })
+
+  if(Board.win == true){
+    alert(Board.colour + " wins!")
+  }
+
+  if(Board.win == true){
+
+    winnerString = board.colour + " wins!"
+
+    function loaded () {
+      document.getElementById("win-message").innerHTML = winnerString;
+    }
+
+    $(function() {
+    $( "#win-message" ).dialog({
+      modal: true,
+      buttons: {
+        New Game: function() {
+          Board.newGame();
+          newBoard();
+          $( this ).dialog( "close" );
+        }
+      }
+    });
+  });
+  }
+
 });
